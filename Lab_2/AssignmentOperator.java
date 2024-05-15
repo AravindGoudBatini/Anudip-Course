518c565dbb0d7addc8636272490631f3d73c1393
Aravind Bathini <aravind463436@gmail.com>
2024-05-15 10:42:32 +0530

    Add files via upload

---
+++ Lab_2/AssignmentOperator.java
@@ -0,0 +1,11 @@
+package Lab_2 ;
+class AssignmentOperator {
+    public static void main(String[] args) {
+        int num = 466;
+        char letter = 'A';
+        System.out.println("Simple assignment:");
+        System.out.println("num1 = " + num);
+        System.out.println("letter = " + letter);
+
+    }
+}
---
+++ Lab_2/UserLoginCheck.java
@@ -0,0 +1,13 @@
+package Lab_2 ;
+public class UserLoginCheck {
+
+    public static void main(String[] args){
+         String login = "Aravind";
+         if(login == "Arav") {
+             System.out.println("user is valid");
+         }
+         else {
+             System.out.println("user is not logged in");
+         }
+    }
+}
