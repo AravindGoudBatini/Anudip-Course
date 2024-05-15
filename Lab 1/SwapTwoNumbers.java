92d2b9849cfbbb09332db1adc73a5bb23f388093
Aravind Bathini <aravind463436@gmail.com>
2024-05-15 10:42:04 +0530

    Add files via upload

---
+++ Lab 1/SwapTwoNumbers.java
@@ -0,0 +1,22 @@
+public class SwapTwoNumbers {
+
+
+    public static void main(String[] args)
+    {
+        int x = 25;
+        int y = 2552;
+    
+        System.out.println("Before swapping:");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    
+        x = x + y;
+        y = x - y;
+        x = x - y;
+    
+        System.out.println("After swapping:");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+    
+    }
